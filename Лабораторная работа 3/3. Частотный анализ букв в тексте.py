a3641befb5347f2f63aa9e73ef8f4fa3fa9fb44d
JustEgor21 <egorp5506@gmail.com>
2024-12-07 16:12:45 +0500

    Лабораторная работа 3

---
+++ Лабораторная работа 3/3. Частотный анализ букв в тексте.py
@@ -0,0 +1,64 @@
+# TODO  Напишите функцию count_letters
+def count_letters(verse):
+    letter_count = {}  # словарь для сложения букв
+    verse = verse.lower()  # приводим буквы к нижнему регистру
+
+    for letter in verse:  # перебираем буквы в списке verse
+        if letter.isalpha():  # если символ это буква, то:
+            if letter in letter_count:
+                letter_count[letter] += 1
+            else:
+                letter_count[letter] = 1
+
+    return letter_count
+# TODO Напишите функцию calculate_frequency
+def calculate_frequency(letter_count):
+    frequency = {}
+    sum_letters = sum(letter_count.values())
+
+    for letter, count in letter_count.items():
+        frequency[letter] = count / sum_letters
+
+    return frequency
+
+main_str = """
+У лукоморья дуб зелёный;
+Златая цепь на дубе том:
+И днём и ночью кот учёный
+Всё ходит по цепи кругом;
+Идёт направо — песнь заводит,
+Налево — сказку говорит.
+Там чудеса: там леший бродит,
+Русалка на ветвях сидит;
+Там на неведомых дорожках
+Следы невиданных зверей;
+Избушка там на курьих ножках
+Стоит без окон, без дверей;
+Там лес и дол видений полны;
+Там о заре прихлынут волны
+На брег песчаный и пустой,
+И тридцать витязей прекрасных
+Чредой из вод выходят ясных,
+И с ними дядька их морской;
+Там королевич мимоходом
+Пленяет грозного царя;
+Там в облаках перед народом
+Через леса, через моря
+Колдун несёт богатыря;
+В темнице там царевна тужит,
+А бурый волк ей верно служит;
+Там ступа с Бабою Ягой
+Идёт, бредёт сама собой,
+Там царь Кащей над златом чахнет;
+Там русский дух… там Русью пахнет!
+И там я был, и мёд я пил;
+У моря видел дуб зелёный;
+Под ним сидел, и кот учёный
+Свои мне сказки говорил.
+"""
+
+letter_count = count_letters(main_str)
+frequency = calculate_frequency(letter_count)
+# TODO Распечатайте в столбик букву и её частоту в тексте
+for letter, count in frequency.items():
+    print(f"{letter}: {count:.2f}")
